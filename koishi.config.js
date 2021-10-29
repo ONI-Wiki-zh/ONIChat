7bf7781c7170816d8a76353f1162f92fb03fc275
Ubuntu <ubuntu@ip-172-31-27-92.ap-southeast-1.compute.internal>
2021-10-29 07:44:49 +0000

    config

--- koishi.config.js
+++ koishi.config.js
@@ -1,15 +1,38 @@
+let secrets = require('./secrets')
+
 // 配置项文档：https://koishi.js.org/api/app.html
-let secrets = require('secrets.js')
 module.exports = {
   // Koishi 服务器监听的端口
   port: 8080,
+  onebot: {
+    secret: '',
+  },
   bots: [{
+    type: 'onebot:ws',
+    // 对应 cqhttp 配置项 ws_config.port
+    server: secrets.onebotServer,
+    selfId: secrets.onebotId,
+    token: secrets.onebotToken,
+  },
+  {
     type: 'discord',
     token: secrets.discordToken,
   }],
   plugins: {
+    mysql: {
+      host: secrets.mysqlHost,
+      // Koishi 服务器监听的端口
+      port: secrets.mysqlPort,
+      user: secrets.mysqlUser,
+      password: secrets.mysqlPassword,
+      database: 'koishi',
+    },
     common: {},
-    github: {},
-    webui: {},
+    teach: {},
+    // webui: {},
   },
+  // 一旦收到来自未知频道的消息，就自动注册频道数据，代理者为收到消息的人
+  autoAssign: true,
+  // 一旦收到来自未知用户的消息，就自动注册用户数据，权限等级为 1
+  autoAuthorize: 1,
 }
