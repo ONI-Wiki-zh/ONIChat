756e8b42d82f4662610cb23a675cb797502db4be
DDElephant <andy-ding@outlook.com>
2022-01-12 23:25:29 -0800

    random test

---
+++ app/src/koishi.config.random.ts
@@ -0,0 +1,104 @@
+import { defineConfig } from '@koishijs/cli';
+import { BotConfig as DCConfig } from '@koishijs/plugin-adapter-discord';
+import {} from '@koishijs/plugin-adapter-onebot';
+// import {} from '@koishijs/plugin-adapter-telegram';
+import {} from '@koishijs/plugin-admin';
+import {} from '@koishijs/plugin-chat';
+import {} from '@koishijs/plugin-common';
+import {} from '@koishijs/plugin-console';
+import {} from '@koishijs/plugin-database-mysql';
+import {} from '@koishijs/plugin-manager';
+import {} from '@koishijs/plugin-status';
+import {} from '@koishijs/plugin-switch';
+import {} from '@koishijs/plugin-teach';
+import {} from '../../packages/koishi-plugin-adapter-minecraft/src/index';
+import { LinkConfig } from './plugins/party-line-phone';
+import secrets from './secrets';
+
+const dcConfig: DCConfig = {
+  token: secrets.yallage.discordToken,
+};
+
+const relayMC: LinkConfig = [
+  {
+    msgPrefix: '【犽之谷】',
+    platform: 'minecraft',
+    usePrefix: true,
+    channelId: '_public',
+    botId: 'DDEle',
+  },
+  {
+    msgPrefix: '【DC】',
+    platform: 'discord',
+    channelId: '930861949506441216',
+    guildId: '888755372217753610',
+    botId: secrets.yallage.discordId,
+    webhookID: secrets.yallage.relayWebhookIDMC,
+    webhookToken: secrets.yallage.relayWebhookTokenMC,
+  },
+];
+
+const linksConfig = [relayMC];
+
+const conf = defineConfig({
+  // Wait until it has access control
+  host: '0.0.0.0',
+  port: 8084,
+  nickname: ['yallage'],
+  plugins: {
+    'adapter-discord': dcConfig,
+    '../../packages/koishi-plugin-adapter-minecraft/src/index': {
+      host: 'server.vcraft.top',
+      username: secrets.yallage.mcUsername,
+      password: secrets.yallage.mcPassword,
+      auth: 'microsoft',
+      version: '1.16.5',
+      rateLimit: 300,
+      receiveMessage: {
+        username: '犽之谷',
+      },
+    },
+    'database-mysql': {
+      host: secrets.mysqlHost,
+      // Koishi 服务器监听的端口
+      port: secrets.mysqlPort,
+      user: secrets.mysqlUser,
+      password: secrets.mysqlPassword,
+      database: 'yallage_v4',
+    },
+    admin: {},
+    common: {
+      // onRepeat: {
+      //   minTimes: 3,
+      //   probability: 0.5,
+      // },
+      // onFriendRequest: true,
+    },
+    // github: {},
+    teach: {
+      prefix: '#',
+      authority: { regExp: 2 },
+    },
+    console: {},
+    manager: {},
+    // status: {},
+    chat: {},
+    './plugins/party-line-phone': { links: linksConfig },
+  },
+  autoAssign: true,
+  autoAuthorize: 1,
+  prefix: ['.', '。'],
+  watch: {
+    // root: 'src', // 要监听的根目录，相对于工作路径
+    // 要忽略的文件列表，支持 glob patterns
+    ignored: ['*.log'],
+  },
+  logger: {
+    levels: {
+      base: 2,
+      rss: 3,
+    },
+    showTime: true,
+  },
+});
+export default conf;
